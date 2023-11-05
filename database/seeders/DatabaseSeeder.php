bd5e2277e5250978af2d4215e3defa807d2ffcbc
Marina <marinalaguna2004@gmail.com>
2023-11-05 15:08:50 +0100

    uso de seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('jugador')->insert([
+                'nombre' => $faker->name,
+                'password' => $faker->passsword(8, 12),
+                'partidasJugadas' => $faker->numberBetween(1, 100),
+                'partidasGanadas' => $faker->numberBetween(1, 50),
+                'rol' => $faker->randomElement(['admin', 'usuario']),
+            ]);
+        }
+    }
+
+    public function calcularPuntuacion($id) {
+        $jugador = DB::table('jugador')->where('id', $id)->first();
+        $puntuacion = $jugador->partidasGanadas * 10; // Suponiendo que cada partida ganada da 10 puntos
+    
+        return response()->json(['puntuacion' => $puntuacion], 200);
     }
 }
